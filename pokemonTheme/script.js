44e59bcf421356b988b0a3f2d246ecc7a294683c
luv Kapur <luvkapur4@gmail.com>
2025-03-14 23:37:51 +0530

    working on pokemon theme page

---
+++ pokemonTheme/script.js
@@ -0,0 +1,7 @@
+let leftBtn = document.querySelector(".btn-left")
+let rightBtn = document.querySelector(".btn-right")
+let img = document.querySelector(".pokeImg");
+
+leftBtn.addEventListener("click", function(){
+    img.
+})
